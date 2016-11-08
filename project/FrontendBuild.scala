38c16930d7668ca8734310c6b6f0a28bc4ec6956
nickpace <pacenick@gmail.com>
2016-11-08 10:13:43 +0000

    NGC-757 - Plat 2.5 upgrade.

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -1,31 +1,29 @@
 import sbt._
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
-
+import play.sbt.PlayImport._
 
 object FrontendBuild extends Build with MicroService {
+  import scala.util.Properties.envOrElse
 
   override val appName = "mobile-token-proxy"
 
-  override lazy val plugins: Seq[Plugins] = Seq(
-    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
-  )
-
-  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()}
+  val appVersion = envOrElse("MOBILE_TOKEN_PROXY_VERSION", "999-SNAPSHOT")
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
 
 private object AppDependencies {
+  import play.sbt.PlayImport._
   import play.core.PlayVersion
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "6.5.0",
-    "uk.gov.hmrc" %% "play-config" % "2.0.1",
-    "uk.gov.hmrc" %% "play-json-logger" % "2.1.1",
-    "uk.gov.hmrc" %% "play-health" % "1.1.0",
-    "uk.gov.hmrc" %% "govuk-template" % "4.0.0",
-    "uk.gov.hmrc" %% "play-ui" % "4.10.0",
-    "uk.gov.hmrc" %% "domain" % "3.5.0",
-    "uk.gov.hmrc" %% "crypto" % "3.1.0"
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.7.0",
+    "uk.gov.hmrc" %% "play-config" % "3.0.0",
+    "uk.gov.hmrc" %% "play-json-logger" % "3.0.0",
+    "uk.gov.hmrc" %% "play-health" % "2.0.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.0.0",
+    "uk.gov.hmrc" %% "play-ui" % "5.2.0",
+    "uk.gov.hmrc" %% "domain" % "4.0.0",
+    "uk.gov.hmrc" %% "play-json-logger" % "3.0.0"
+
   )
 
   trait TestDependencies {
@@ -37,10 +35,10 @@ private object AppDependencies {
     def apply() = new TestDependencies {
 
       override lazy val test = Seq(
-        "org.scalatest" %% "scalatest" % "2.2.2" % scope,
-        "org.pegdown" % "pegdown" % "1.4.2" % scope,
+        "org.scalatest" %% "scalatest" % "3.0.0" % scope,
+        "org.pegdown" % "pegdown" % "1.6.0" % scope,
         "org.jsoup" % "jsoup" % "1.7.3" % scope,
-        "uk.gov.hmrc" %% "hmrctest" % "1.4.0" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "2.0.0" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
       )
     }.test
@@ -52,8 +50,8 @@ private object AppDependencies {
       override lazy val scope = "it"
 
       override lazy val test = Seq(
-        "org.scalatest" %% "scalatest" % "2.2.2" % scope,
-        "org.pegdown" % "pegdown" % "1.4.2" % scope,
+        "org.scalatest" %% "scalatest" % "3.0.0" % scope,
+        "org.pegdown" % "pegdown" % "1.6.0" % scope,
         "org.jsoup" % "jsoup" % "1.7.3" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
       )
@@ -62,8 +60,3 @@ private object AppDependencies {
 
   def apply() = compile ++ Test() ++ IntegrationTest()
 }
-
-
-
-
-
