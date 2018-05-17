0f281932997f8b0139b6d330110e7dbaad4c3686
Paul Hodgson <paul.hodgson@digital.hmrc.gov.uk>
2018-05-17 10:50:52 +0100

    [NGC-3236][ph] upgraded library versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ object AppDependencies {
   import play.core.PlayVersion
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "8.9.0",
-    "uk.gov.hmrc" %% "domain" % "5.0.0"
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "8.20.0",
+    "uk.gov.hmrc" %% "domain" % "5.1.0"
   )
 
   trait TestDependencies {
@@ -18,7 +18,6 @@ object AppDependencies {
 
       override lazy val test = Seq(
         "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
-        "org.jsoup" % "jsoup" % "1.10.1" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
       )
     }.test
@@ -30,7 +29,6 @@ object AppDependencies {
       override lazy val scope = "it"
 
       override lazy val test = Seq(
-        "org.scalatest" %% "scalatest" % "3.0.1" % scope,
         "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
