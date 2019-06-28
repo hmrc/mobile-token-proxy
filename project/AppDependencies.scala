44f5fe33806c045ded11a0e9580a0991748ffc7d
paulomiguel010 <paulomiguel010@gmail.com>
2019-06-28 10:29:07 +0100

    Dependencies updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,9 +6,9 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-play-26" % "0.35.0",
     "uk.gov.hmrc" %% "govuk-template"    % "5.27.0-play-26",
-    "uk.gov.hmrc" %% "play-ui"           % "7.27.0-play-26",
+    "uk.gov.hmrc" %% "play-ui"           % "7.40.0-play-26",
     "uk.gov.hmrc" %% "play-hmrc-api"     % "3.4.0-play-26",
-    "uk.gov.hmrc" %% "domain"            % "5.3.0"
+    "uk.gov.hmrc" %% "domain"            % "5.6.0-play-26"
   )
 
   trait TestDependencies {
